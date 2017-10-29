fad266cba1cb2186daeec4b9805d15209b1f417b
Ugur Armagan <ugurarmagan93@gmail.com>
2017-10-29 16:46:10 -0500

    fix error in prod key

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // prod.js - production keys here
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientID: PROCESS.ENV.GOOGLE_CLIENT_ID,
   googleClientSecret: PROCESS.ENV.GOOGLE_CLIENT_SECRET,
   mongoURI: PROCESS.ENV.MONGO_URI,
   cookieKey: PROCESS.ENV.COOKIE_KEY
