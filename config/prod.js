cd295dbf52c1dc6ba2ffb77951b7b754a676b8b5
Ugur Armagan <ugurarmagan93@gmail.com>
2017-10-29 16:37:24 -0500

    finish auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod.js - production keys here
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: PROCESS.ENV.GOOGLE_CLIENT_SECRET,
+  mongoURI: PROCESS.ENV.MONGO_URI,
+  cookieKey: PROCESS.ENV.COOKIE_KEY
+};
